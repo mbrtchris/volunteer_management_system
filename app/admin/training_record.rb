ecc0960491f600b2b4c47dcb75b9b47fda7f8e14
Prelang Builder <builder@prelang.com>
2016-12-29 19:02:54 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'TrainingRecord' to allow for create, update

--- app/admin/training_record.rb
+++ app/admin/training_record.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register TrainingRecord do
+  permit_params :type, :has_passed, :expires_at
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
