063cc9bf4749e904f17d9aa1c4581aea6b901f7a
Prelang Builder <builder@prelang.com>
2016-12-29 19:03:31 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Location' to allow for create, update

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Location do
+  permit_params :address1, :address2, :city, :state, :zipcode, :is_mailing
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
